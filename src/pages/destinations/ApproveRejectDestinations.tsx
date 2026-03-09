226a1fc51932acd23b18ae4be6ad851a7d66d2cd
Anshuman Singh Negi <anshuman.negi@paynways.com>
2026-03-09 19:01:42 +0530

    push

--- src/pages/destinations/ApproveRejectDestinations.tsx
+++ src/pages/destinations/ApproveRejectDestinations.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { MapPin, Check, X } from "lucide-react";
+import { MapPin} from "lucide-react";
 import toast from "react-hot-toast";
 import {
   fetchAdminDestinations
